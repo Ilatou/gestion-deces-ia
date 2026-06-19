3c0b9b5f021b62837023733e43304b23f74893ae
Ilatou <msoul7581@gmail.com>
2026-06-19 16:35:15 +0100

    Configuration de l'administration Django pour les utilisateurs et les décès

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+    )
+
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Informations supplémentaires",
+            {
+                "fields": (
+                    "role",
+                    "telephone",
+                    "date_creation",
+                )
+            },
+        ),
+    )
+
+    readonly_fields = (
+        "date_creation",
+    )
--- backend/deces/admin.py
+++ backend/deces/admin.py
@@ -1,3 +1,115 @@
 from django.contrib import admin
+from .models import CauseDeces, Lieu, Personne, Deces
 
-# Register your models here.
+
+@admin.register(CauseDeces)
+class CauseDecesAdmin(admin.ModelAdmin):
+    list_display = (
+        "code_cim",
+        "libelle",
+        "categorie",
+    )
+
+    search_fields = (
+        "code_cim",
+        "libelle",
+    )
+
+    list_filter = (
+        "categorie",
+    )
+
+    ordering = (
+        "code_cim",
+    )
+
+
+@admin.register(Lieu)
+class LieuAdmin(admin.ModelAdmin):
+    list_display = (
+        "pays",
+        "region",
+        "ville",
+        "etablissement",
+    )
+
+    search_fields = (
+        "pays",
+        "region",
+        "ville",
+        "etablissement",
+    )
+
+    list_filter = (
+        "pays",
+        "region",
+    )
+
+    ordering = (
+        "pays",
+        "region",
+    )
+
+
+@admin.register(Personne)
+class PersonneAdmin(admin.ModelAdmin):
+    list_display = (
+        "nom",
+        "prenom",
+        "sexe",
+        "age",
+    )
+
+    search_fields = (
+        "nom",
+        "prenom",
+    )
+
+    list_filter = (
+        "sexe",
+    )
+
+    ordering = (
+        "nom",
+        "prenom",
+    )
+
+
+@admin.register(Deces)
+class DecesAdmin(admin.ModelAdmin):
+    list_display = (
+        "personne",
+        "cause",
+        "lieu",
+        "date_deces",
+        "type_deces",
+        "certificat_medical",
+        "enregistre_par",
+    )
+
+    search_fields = (
+        "personne__nom",
+        "personne__prenom",
+        "cause__libelle",
+        "cause__code_cim",
+    )
+
+    list_filter = (
+        "type_deces",
+        "date_deces",
+        "certificat_medical",
+        "lieu__region",
+    )
+
+    autocomplete_fields = (
+        "personne",
+        "cause",
+        "lieu",
+        "enregistre_par",
+    )
+
+    date_hierarchy = "date_deces"
+
+    ordering = (
+        "-date_deces",
+    )
